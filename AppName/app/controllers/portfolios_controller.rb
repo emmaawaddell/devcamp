b6de490c0284dee4d75ab4bc50fb4d15d378038c
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 15:56:51 +0000

    Integrated new and create actins for portfolios

--- AppName/app/controllers/portfolios_controller.rb
+++ AppName/app/controllers/portfolios_controller.rb
@@ -2,4 +2,21 @@ class PortfoliosController < ApplicationController
  def index
    @portfolio_items = Portfolio.all
  end
-  end
+  
+ def new
+  @portfolio_item = Portfolio.new
+ end
+ 
+ def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    
+    respond_to do |format|
+     if @blog.save
+      format.html {redirct_to @blog, notice: 'Your post is now live.'
+     else
+        format.html { render :new }
+           end
+          end
+         end
+         
+        end
