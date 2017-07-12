19935c2d411dcf88bef7b932e7ebd639de58c863
emmaawaddell <emmaloufa@gmail.com>
2017-07-12 17:53:50 +0000

    Added guest user for current_user

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,6 +1,6 @@
-# Be sure to restart your server when you modify this file.
-
-# ApplicationController.renderer.defaults.merge!(
-#   http_host: 'example.org',
-#   https: false
-# )
+  class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    include DeviseWhitelist
+    include SetSource
+    include CurrentUserConcern
+  end
